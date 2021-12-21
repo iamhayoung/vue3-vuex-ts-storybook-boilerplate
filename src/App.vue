5ad07eaa16626ab4491f95cbebd72e333a1764c3
Hayoung Gam <hahhyoung@gmail.com>
2021-12-21 23:36:51 +0900

    Add: Init vue3 & TS & vuex & storybook

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <h1>Hello</h1>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+  name: 'App',
+  components: {},
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '@/assets/scss/_normalize.scss';
+@import '@/assets/scss/_global.scss';
 </style>
